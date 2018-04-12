b8dcee8d79b2561af79f7af3150c80a64fcbfe64
TARDE <caiolobato@hotmail.com>
2018-04-12 18:00:09 -0300

    Commit do dia 12/04. Finalizando a tela de Login

--- app/src/main/java/pa/senac/br/greencoin/AppActivity.java
+++ app/src/main/java/pa/senac/br/greencoin/AppActivity.java
@@ -1,13 +1,37 @@
 package pa.senac.br.greencoin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class AppActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class AppActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app);
+
+        findViewById(R.id.logout_button).setOnClickListener(this);
+
+
     }
+
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.logout_button) {
+            Toast.makeText(this,"Deslogou",Toast.LENGTH_LONG).show();
+            mAuth.signOut();
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
 }
--- app/src/main/java/pa/senac/br/greencoin/MainActivity.java
+++ app/src/main/java/pa/senac/br/greencoin/MainActivity.java
@@ -1,12 +1,16 @@
 package pa.senac.br.greencoin;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.VideoView;
 
@@ -21,66 +25,63 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity implements
         View.OnClickListener {
 
-    private VideoView mVideoView;
+    private EditText mEmailField;
+    private EditText mPasswordField;
+    private ProgressDialog progressDialog;
+
 
     private FirebaseAuth mAuth;
+
+    private VideoView mVideoView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        progressDialog = new ProgressDialog(MainActivity.this);
+
+        mEmailField = findViewById(R.id.field_email);
+        mPasswordField = findViewById(R.id.field_password);
+
         findViewById(R.id.wechatLoginButton).setOnClickListener(this);
         findViewById(R.id.fbLoginButton).setOnClickListener(this);
         findViewById(R.id.otherAccButton).setOnClickListener(this);
         findViewById(R.id.createAccountButton).setOnClickListener(this);
-
+        findViewById(R.id.loginButton).setOnClickListener(this);
 
 
         //In the onCreate() method, initialize the FirebaseAuth instance.
         mAuth = FirebaseAuth.getInstance();
-
+        mAuth.signOut();
         //mAuth.createUserWithEmailAndPassword("caiolobatogon@gmail.com","12345678");
-
-        verificaUserLogado();
 
         videoBG();
 
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        //FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+        verificaUserLogado();
+    }
 
 
     private void verificaUserLogado() {
         //Verificar usuario logado
         if(mAuth.getCurrentUser()!=null){
-
+            Intent intent = new Intent(MainActivity.this,AppActivity.class);
+            startActivity(intent);
             Log.i("verificarUsuario","Usuario está logado");
-
         }else {
+
             Log.i("verificarUsuario","Usuario não está logado");
         }
     }
-
-
-    /*private void updateUI(FirebaseUser user) {
-        //hideProgressDialog();
-        if (user != null) {
-
-            findViewById(R.id.email_password_buttons).setVisibility(View.GONE);
-            findViewById(R.id.email_password_fields).setVisibility(View.GONE);
-            findViewById(R.id.signed_in_buttons).setVisibility(View.VISIBLE);
-
-            findViewById(R.id.verify_email_button).setEnabled(!user.isEmailVerified());
-        } else {
-            mStatusTextView.setText(R.string.signed_out);
-            mDetailTextView.setText(null);
-
-            findViewById(R.id.email_password_buttons).setVisibility(View.VISIBLE);
-            findViewById(R.id.email_password_fields).setVisibility(View.VISIBLE);
-            findViewById(R.id.signed_in_buttons).setVisibility(View.GONE);
-        }
-    }*/
-
 
     @Override
     public void onClick(View v) {
@@ -90,22 +91,133 @@ public class MainActivity extends AppCompatActivity implements
         } else if (i == R.id.fbLoginButton) {
             Toast.makeText(this,"Facebook",Toast.LENGTH_SHORT).show();
         } else if (i == R.id.otherAccButton) {
-            criarContaUI();
+            criarContaUpdateUI();
             Toast.makeText(this,"Outras Contas",Toast.LENGTH_SHORT).show();
         } else if (i== R.id.createAccountButton){
+            criarConta(mEmailField.getText().toString(), mPasswordField.getText().toString());
             Toast.makeText(this,"Criar conta",Toast.LENGTH_SHORT).show();
+        } else if (i== R.id.loginButton){
+            logar(mEmailField.getText().toString(), mPasswordField.getText().toString());
+            Toast.makeText(this,"Logar",Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void criarContaUI() {
+    private void criarConta(String email, String password) {
+        Log.d("createUser", "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        progressDialog.setMessage("Carregando");
+        progressDialog.show();
+
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("createUser", "createUserWithEmail:success");
+                            verificaUserLogado();
+                            //FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("createUser", "createUserWithEmail:failure", task.getException());
+
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+//                        hideProgressDialog();
+                        progressDialog.hide();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+    private void logar(String email, String password) {
+        Log.d("signIn", "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+//        showProgressDialog();
+        progressDialog.setMessage("Carregando");
+        progressDialog.show();
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("signIn", "signInWithEmail:success");
+                            verificaUserLogado();
+                            //FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("signIn", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+//                        if (!task.isSuccessful()) {
+//                            mStatusTextView.setText(R.string.auth_failed);
+//                        }
+//                        hideProgressDialog();
+                        progressDialog.hide();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+
+
+
+    private void criarContaUpdateUI() {
         findViewById(R.id.field_email).setVisibility(View.VISIBLE);
         findViewById(R.id.field_password).setVisibility(View.VISIBLE);
+        findViewById(R.id.loginButton).setVisibility(View.VISIBLE);
         findViewById(R.id.createAccountButton).setVisibility(View.VISIBLE);
 
         findViewById(R.id.wechatLoginButton).setVisibility(View.INVISIBLE);
         findViewById(R.id.fbLoginButton).setVisibility(View.INVISIBLE);
         findViewById(R.id.otherAccButton).setVisibility(View.INVISIBLE);
     }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = mEmailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEmailField.setError("Campo necessário.");
+            valid = false;
+        } else {
+            mEmailField.setError(null);
+        }
+
+        String password = mPasswordField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mPasswordField.setError("Campo necessário.");
+            valid = false;
+        } else {
+            mPasswordField.setError(null);
+        }
+
+        return valid;
+    }
+
 
     //---------video
     private void videoBG() {
