5a1e560210101abd8ac2c81a01d768200ef81137
caiolobato <caiolobato@hotmail.com>
2018-04-12 06:07:14 -0300

    Adicionado a UI de criar conta email e senha

--- app/src/main/java/pa/senac/br/greencoin/MainActivity.java
+++ app/src/main/java/pa/senac/br/greencoin/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 import android.widget.VideoView;
 
@@ -14,45 +15,108 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        View.OnClickListener {
 
     private VideoView mVideoView;
 
     private FirebaseAuth mAuth;
-    private String TAG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.wechatLoginButton).setOnClickListener(this);
+        findViewById(R.id.fbLoginButton).setOnClickListener(this);
+        findViewById(R.id.otherAccButton).setOnClickListener(this);
+        findViewById(R.id.createAccountButton).setOnClickListener(this);
+
+
+
         //In the onCreate() method, initialize the FirebaseAuth instance.
         mAuth = FirebaseAuth.getInstance();
 
-//        mAuth.createUserWithEmailAndPassword("caio.lobato@gmail.com","caio.lobato123")
-//                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<AuthResult> task) {
-//
-//                        if(task.isSuccessful()){
-//                            //cadastro com sucesso
-//                            Log.i("createUser", "Sucesso ao cadastrar o usuario!!!");
-//                        } else {
-//                            //erro ao cadastrar
-//                            Log.i("createUser","Erro ao cadastrar o usuario!!!");
-//                        }
-//
-//                    }
-//                });
+        //mAuth.createUserWithEmailAndPassword("caiolobatogon@gmail.com","12345678");
 
+        verificaUserLogado();
+
+        videoBG();
+
+    }
+
+
+
+    private void verificaUserLogado() {
+        //Verificar usuario logado
+        if(mAuth.getCurrentUser()!=null){
+
+            Log.i("verificarUsuario","Usuario está logado");
+
+        }else {
+            Log.i("verificarUsuario","Usuario não está logado");
+        }
+    }
+
+
+    /*private void updateUI(FirebaseUser user) {
+        //hideProgressDialog();
+        if (user != null) {
+
+            findViewById(R.id.email_password_buttons).setVisibility(View.GONE);
+            findViewById(R.id.email_password_fields).setVisibility(View.GONE);
+            findViewById(R.id.signed_in_buttons).setVisibility(View.VISIBLE);
+
+            findViewById(R.id.verify_email_button).setEnabled(!user.isEmailVerified());
+        } else {
+            mStatusTextView.setText(R.string.signed_out);
+            mDetailTextView.setText(null);
+
+            findViewById(R.id.email_password_buttons).setVisibility(View.VISIBLE);
+            findViewById(R.id.email_password_fields).setVisibility(View.VISIBLE);
+            findViewById(R.id.signed_in_buttons).setVisibility(View.GONE);
+        }
+    }*/
+
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.wechatLoginButton) {
+            Toast.makeText(this,"Google",Toast.LENGTH_LONG).show();
+        } else if (i == R.id.fbLoginButton) {
+            Toast.makeText(this,"Facebook",Toast.LENGTH_SHORT).show();
+        } else if (i == R.id.otherAccButton) {
+            criarContaUI();
+            Toast.makeText(this,"Outras Contas",Toast.LENGTH_SHORT).show();
+        } else if (i== R.id.createAccountButton){
+            Toast.makeText(this,"Criar conta",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void criarContaUI() {
+        findViewById(R.id.field_email).setVisibility(View.VISIBLE);
+        findViewById(R.id.field_password).setVisibility(View.VISIBLE);
+        findViewById(R.id.createAccountButton).setVisibility(View.VISIBLE);
+
+        findViewById(R.id.wechatLoginButton).setVisibility(View.INVISIBLE);
+        findViewById(R.id.fbLoginButton).setVisibility(View.INVISIBLE);
+        findViewById(R.id.otherAccButton).setVisibility(View.INVISIBLE);
+    }
+
+    //---------video
+    private void videoBG() {
         //Codigo do video começa aqui
-        mVideoView = (VideoView) findViewById(R.id.bgVideoView);
+        mVideoView = findViewById(R.id.bgVideoView);
 
         Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.bg_video);
 
         mVideoView.setVideoURI(uri);
         mVideoView.start();
+
 
         mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
@@ -62,15 +126,6 @@ public class MainActivity extends AppCompatActivity {
         });
         //Codigo do video termina aqui
     }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        //updateUI(currentUser);
-    }
-
 
 
 }
